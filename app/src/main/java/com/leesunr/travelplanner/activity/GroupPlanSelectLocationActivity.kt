a08c80422eced1ac5091ec09440831607a4a612a
leesunr <leesunr@qlcinc.co.kr>
2020-10-23 18:53:58 +0900

    add google map marker

--- app/src/main/java/com/leesunr/travelplanner/activity/GroupPlanSelectLocationActivity.kt
+++ app/src/main/java/com/leesunr/travelplanner/activity/GroupPlanSelectLocationActivity.kt
@@ -1,20 +1,26 @@
 package com.leesunr.travelplanner.activity
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.leesunr.travelplanner.R
 
 
-class GroupPlanSelectLocationActivity : AppCompatActivity(), OnMapReadyCallback {
+class GroupPlanSelectLocationActivity : AppCompatActivity(), OnMapReadyCallback{
 
     private lateinit var mMap: GoogleMap
-
+    private lateinit var marker: Marker
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_group_plan_select_location)
@@ -25,14 +31,21 @@ class GroupPlanSelectLocationActivity : AppCompatActivity(), OnMapReadyCallback 
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(
-            MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney")
-        )
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            val sydney = LatLng(it.latitude, it.longitude)
+            marker = mMap.addMarker(MarkerOptions().position(mMap.cameraPosition.target))
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,15.0f))
+        }
     }
 }
